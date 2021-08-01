3be25e903d19968b569eba78fef1b9da85d8b624
zcsEason <527095583@qq.com>
2021-08-01 21:19:16 +0800

    init 添加TextBlockEx

---
+++ ControlEx/TextBlockEx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ControlEx
+{
+    public class TextBlockEx:TextBlock
+    {
+
+        /// <summary>
+        /// 当前文字是否被截取
+        /// </summary>
+
+        public bool IsTextTrimming
+        {
+            get { return (bool)GetValue(IsTextTrimmingProperty); }
+            private set { SetValue(IsTextTrimmingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsTextTrimming.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsTextTrimmingProperty =
+            DependencyProperty.Register("IsTextTrimming", typeof(bool), typeof(TextBlock), new PropertyMetadata(false));
+
+        protected override Geometry GetLayoutClip(Size layoutSlotSize)
+        {
+            IsTextTrimming = GetIsTrimming();
+            return base.GetLayoutClip(layoutSlotSize);
+        }
+
+         bool GetIsTrimming()
+        {
+            if (TextTrimming == TextTrimming.None) return false;
+
+            var measure = new MeasureTool();
+            measure.Width = RenderSize.Width;
+            measure.Height = RenderSize.Height;
+            var needSize = measure.GetContentRenderSize(this);
+
+            if (TextWrapping == TextWrapping.NoWrap)  //比较长度
+            {
+                if (needSize.Width > RenderSize.Width) return true;
+            }
+            else  //比较高度
+            {
+                if (needSize.Height > RenderSize.Height) return true;
+            }
+            return false;
+        }
+    }
+
+    class MeasureTool : TextBlock
+    {
+        public Size GetContentRenderSize(TextBlock target)
+        {
+            Text = target.Text;
+            TextWrapping = target.TextWrapping;
+            TextTrimming = TextTrimming.None;
+
+            FontFamily = target.FontFamily;
+            FontSize = target.FontSize;
+            FontStyle = target.FontStyle;
+            FontStretch = target.FontStretch;
+
+            return MeasureOverride(target.RenderSize);
+        }
+    }
+}
---
+++ ControlExTest/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlExTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //https://www.cnblogs.com/kybs0/p/7305440.html
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        tb.Text += "死快点回来卡";
+                        if (tb.Text.Length > 100)
+                            tb.Text = "";
+                    });
+                    System.Threading.Thread.Sleep(500);
+                }
+            });
+        }
+    }
+}
